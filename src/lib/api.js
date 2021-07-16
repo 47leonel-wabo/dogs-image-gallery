185cb1d63571b299c8f3d90dc588e62d182c936f
leonel47 <waboleonel@gmail.com>
2021-07-15 20:45:32 -0400

    Using environment variable for key protection

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // const API_URL = process.env.REACT_APP_DOG_API_URL
-const API_URL = 'https://api.thedogapi.com/v1/'
-const API_KEY = 'ba39ed54-d7bb-4aac-a680-c1f5d12d7d82'
+const API_URL = process.env.REACT_APP_DOG_API_BASE_URL
+const API_KEY = process.env.REACT_APP_DOG_API_KEY
 // const API_KEY = process.env.REACT_APP_DOG_API_KEY
 
 /*
